20239fa57906574d188b47972037272600584882
Slim <99382979+aaymecho@users.noreply.github.com>
2024-09-21 20:50:09 -0400

    Feature/1 creating repo interfaces (#3)
    * updated readme for data model interactions
    
    * added payroll and employee data models
    
    * modified application properties + added employee and payroll model
    
    * added repos for models
    
    * shifted employee repo methods to payroll repo

--- src/main/java/com/gatech/payroll/EmployeePayrollSystem/repository/PayrollRepository.java
+++ src/main/java/com/gatech/payroll/EmployeePayrollSystem/repository/PayrollRepository.java
@@ -1,6 +1,5 @@
 package com.gatech.payroll.EmployeePayrollSystem.repository;
 
-import com.gatech.payroll.EmployeePayrollSystem.model.Employee;
 import com.gatech.payroll.EmployeePayrollSystem.model.Payroll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +9,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface EmployeeRepository {
+public interface PayrollRepository extends JpaRepository<Payroll, Long> {
+    List<Payroll> findByEmployeeId(Long employeeId);
 
+    Payroll findByEmployeeIdAndPayPeriod(Long employeeId, String payPeriod);
+
+    @Query("select case when count(p) > 0 then true else false end from Payroll p where p.payPeriod = :payPeriod")
+    Boolean existsByPayPeriod(@Param("payPeriod") String payPeriod);
 }
